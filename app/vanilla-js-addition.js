d2ff9ccb133ff09987ff38798ab963ab1e74e673
Dan Kiss <flowfast47@gmail.com>
2017-11-06 12:05:47 -0800

    add code that triggers bug for testing purposes, resolve bug

--- app/vanilla-js-addition.js
+++ app/vanilla-js-addition.js
@@ -1,4 +1,15 @@
-var firstNumber = prompt('please enter a number');
-var secondNumber = prompt ('please enter another number');
-var sum = firstNumber + secondNumber;
-alert('The sum of your two numbers is: ' + sum);
+var findSum = function(first,second){
+  var sum = first+second;
+  alert('the sum of your two numbers is '+sum);
+}
+
+var number = parseInt(prompt('please enter a number'));
+var otherNumber = parseInt(prompt('enter another number'));
+findSum(number);
+
+
+// old code below
+// var firstNumber = prompt('please enter a number');
+// var secondNumber = prompt ('please enter another number');
+// var sum = firstNumber + secondNumber;
+// alert('The sum of your two numbers is: ' + sum);
