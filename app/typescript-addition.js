ee0b274e48d0a262bc402cf84549daf4d54b5fc4
Dan Kiss <flowfast47@gmail.com>
2017-11-06 11:58:47 -0800

    update javascript to use a function to add numbers

--- app/typescript-addition.js
+++ app/typescript-addition.js
@@ -1,4 +1,12 @@
+var findSum = function (firstValue, secondValue) {
+    var sum = firstValue + secondValue;
+    alert('The sum of your two numbers is: ' + sum);
+};
 var firstNumber = parseInt(prompt('please enter a number'));
 var secondNumber = parseInt(prompt('enter another number'));
-var sum = firstNumber + secondNumber;
-alert('the sum of your two numbers is: ' + sum);
+findSum(firstNumber, secondNumber);
+// the code below was original code
+// var firstNumber: number = parseInt(prompt('please enter a number'));
+// var secondNumber: number = parseInt(prompt('enter another number'));
+// var sum: number = firstNumber + secondNumber;
+// alert('the sum of your two numbers is: '+sum);
--- app/typescript-addition.ts
+++ app/typescript-addition.ts
@@ -1,8 +1,15 @@
+var findSum = function(firstValue: number, secondValue: number) {
+  var sum: number = firstValue + secondValue;
+  alert('The sum of your two numbers is: '+sum);
+}
 
+var firstNumber = parseInt(prompt('please enter a number'));
+var secondNumber = parseInt(prompt('enter another number'));
+findSum(firstNumber, secondNumber);
 
 
 // the code below was original code
-var firstNumber: number = parseInt(prompt('please enter a number'));
-var secondNumber: number = parseInt(prompt('enter another number'));
-var sum: number = firstNumber + secondNumber;
-alert('the sum of your two numbers is: '+sum);
+// var firstNumber: number = parseInt(prompt('please enter a number'));
+// var secondNumber: number = parseInt(prompt('enter another number'));
+// var sum: number = firstNumber + secondNumber;
+// alert('the sum of your two numbers is: '+sum);
